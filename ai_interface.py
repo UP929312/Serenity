774fd1cecffa9715083a77b6f3356d76cc2da2e1
UP929312 <36114463+UP929312@users.noreply.github.com>
2023-05-16 21:20:29 +0100

    AI interface progress

--- ai_interface.py
+++ ai_interface.py
@@ -1,46 +1,37 @@
-import openai
-
-with open("keys/openai_key.txt", "r") as file:
-    openai.api_key = file.read().strip()
-
-"""
-messages = [{"role": "system", "content":
-             "You are a virtual therapist. You will do your best to guide and advise the user on how to progress. "
-             "You will ask questions about the user, especially when you're unsure of anything."}]
-
-while True:
-    message = input("User: ")
-    if message:
-        messages.append({"role": "user", "content": message})
-        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    convert_text_to_speech(reply, play_message=True)
-    messages.append({"role": "assistant", "content": reply})
-"""
 from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
+from langchain.agents import load_tools
 from langchain.memory import ConversationBufferWindowMemory
 
+from prompts import prompts
 
-template = """
-Assistant is a large language model trained by OpenAI.
-Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+with open("keys/openai_key.txt", "r") as file:
+    api_key = file.read().strip()
 
-{history}
-Human: {human_input}
-Assistant:"""
 
-prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
+class AgentInterface:
+    def __init__(self) -> None:
+        template = prompts["starter_prompt"]+"""
 
-chatgpt_chain = LLMChain(
-    llm=OpenAI(temperature=0), 
-    prompt=prompt, 
-    verbose=True, 
-    memory=ConversationBufferWindowMemory(k=2),
-)
+        {history}
+        Human: {human_input}
+        Serenity:""".replace("        ", "")
 
-human_input = "I want you to act as a Linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets {like this}. My first command is pwd."
-output = chatgpt_chain.predict(human_input=human_input)
-print(output)
+        prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
+
+        self.chatgpt_chain = LLMChain(
+            llm=OpenAI(temperature=0, openai_api_key=api_key), 
+            prompt=prompt, 
+            verbose=True, 
+            memory=ConversationBufferWindowMemory(k=2),
+        )
+        tools = load_tools(["wikipedia"], llm=self.chatgpt_chain)
+
+    def continue_chain(self, human_input) -> str:
+        output = self.chatgpt_chain.predict(human_input=human_input)
+        return output
+
+agent = AgentInterface()
+while True:
+    human_input = input("Human: ")
+    output = agent.continue_chain(human_input)
+    print(output)
