c36921781511f773d36d5b07e4a5868d77e3d0ed
UP929312 <36114463+UP929312@users.noreply.github.com>
2023-05-17 14:11:44 +0100

    More blackening

--- detect_facial_expression.py
+++ detect_facial_expression.py
@@ -3,41 +3,44 @@ from hsemotion.facial_emotions import HSEmotionRecognizer
 
 EMOTION_NAMES = ("Anger", "Contempt", "Disgust", "Fear", "Happiness", "Neutral", "Sadness", "Surprise")
 
+
 def test_camera_accessible() -> bool:
     try:
         camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         _, image = camera.read()
         camera.release()
         cv2.imshow("Camera accessible", image)
-        #cv2.waitKey(0)
+        # cv2.waitKey(0)
         cv2.destroyAllWindows()
-        #print("All worked fine")
+        # print("All worked fine")
         return True
     except:
         return False
 
+
 def get_facial_emotion() -> tuple[str, dict[str, float]]:
     try:
-        #print("Taking picture!")
+        # print("Taking picture!")
         camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        #print("Camera made")
+        # print("Camera made")
         _, frame = camera.read()
-        #print("Camera taken")
+        # print("Camera taken")
         camera.release()
-        #print("Release")
+        # print("Release")
     except:
         return "Unknown", {emotion: 0.0 for emotion in EMOTION_NAMES}
-    #print("Taken!")
+    # print("Taken!")
 
-    #cv2.imshow("test", frame)
-    #cv2.waitKey(0)
+    # cv2.imshow("test", frame)
+    # cv2.waitKey(0)
 
-    model_name = 'enet_b0_8_best_afew'
-    fer = HSEmotionRecognizer(model_name=model_name,device='cpu') # device is cpu or gpu
+    model_name = "enet_b0_8_best_afew"
+    fer = HSEmotionRecognizer(model_name=model_name, device="cpu")  # device is cpu or gpu
     emotion, scores = fer.predict_emotions(frame, logits=True)
     scores = dict(list(zip(EMOTION_NAMES, scores)))
     sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
     return emotion, sorted_scores
+
 
 if __name__ == "__main__":
     print(get_facial_emotion())
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,4 +1,3 @@
-
 # https://www.assemblyai.com/docs/Models/entity_detection
 
 import websockets
@@ -6,7 +5,7 @@ import asyncio
 import base64
 import json
 
-#from pyaudio_interface import FRAMES_PER_BUFFER
+# from pyaudio_interface import FRAMES_PER_BUFFER
 
 with open("keys/assemblyai_key.txt", "r") as file:
     auth_key = file.read()
@@ -17,11 +16,12 @@ URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
 # "format_text": False  <- Removes punctuation, to stop sentences being broken up incorrectly
 # "disfluencies": True  <- Keeps the "ums" and "uhs"
 
+
 async def send(_ws, audio_data):
     while True:
         try:
             data = base64.b64encode(audio_data).decode("utf-8")
-            #print(f"{data=}")
+            # print(f"{data=}")
             json_data = json.dumps({"audio_data": str(data)})
             await _ws.send(json_data)
         except websockets.exceptions.ConnectionClosedError as e:
@@ -33,29 +33,32 @@ async def send(_ws, audio_data):
         await asyncio.sleep(0.01)
     return True
 
+
 async def receive(_ws):
     while True:
         try:
             result_str = await _ws.recv()
-            print(json.loads(result_str)['text'])
+            print(json.loads(result_str)["text"])
         except websockets.exceptions.ConnectionClosedError as e:
             print(e)
             assert e.code == 4008
             break
         except Exception as e:
             assert False, "Not a websocket 4008 error"
-      
+
 
 async def send_receive(audio_data: bytes):
-   print(f'Connecting websocket to url ${URL}')
+    print(f"Connecting websocket to url ${URL}")
 
-   async with websockets.connect(URL, extra_headers=(("Authorization", auth_key),), ping_interval=5, ping_timeout=20) as _ws:
-       await asyncio.sleep(0.1)
-       await _ws.recv()
-       send_result, receive_result = await asyncio.gather(send(_ws, audio_data), receive(_ws))
+    async with websockets.connect(URL, extra_headers=(("Authorization", auth_key),), ping_interval=5, ping_timeout=20) as _ws:
+        await asyncio.sleep(0.1)
+        await _ws.recv()
+        send_result, receive_result = await asyncio.gather(send(_ws, audio_data), receive(_ws))
 
-#while True:
+
+# while True:
 #    asyncio.run(send_receive())
+
 
 async def convert_speech_to_text(audio_data: bytes) -> str:
     asyncio.run(send_receive(audio_data))
