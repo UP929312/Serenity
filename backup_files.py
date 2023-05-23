d9d2e64a9e1b60f579a9f3b7bc2e210a25388e9b
UP929312 <36114463+UP929312@users.noreply.github.com>
2023-05-23 14:58:02 +0100

    Changed TTS to be ElevenLabs, which produces incredible results compared to AWS Polly

--- backup_files.py
+++ backup_files.py
@@ -106,3 +106,39 @@ class STTWebhookHandler:
             print(f"{error_message=}")
 
 """
+
+'''
+with open("keys/aws_access_keys.txt", "r") as file:
+    aws_access_key_id, aws_secret_access_key, aws_session_token = (
+        *file.read().strip().split("\n"),
+        None,
+    )
+
+client = boto3.client(
+    "polly",
+    region_name="eu-west-2",
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    aws_session_token=aws_session_token,
+)
+
+
+def convert_text_to_speech(message: str, play_message: bool) -> None:
+    """Convert text to speech using AWS Polly, and play the message if play_message is True"""
+    response = client.synthesize_speech(
+        Engine="neural",
+        LanguageCode="en-GB",
+        # "LexiconNames"=[ "string" ],
+        OutputFormat="mp3",
+        # "SampleRate"="string",
+        # SpeechMarkTypes"=[ "string" ],
+        Text=message,
+        TextType="text",
+        VoiceId="Amy",
+    )
+    streaming_body = response["AudioStream"]
+    with open("assets/audio/main.mp3", "wb") as file:
+        file.write(streaming_body.read())
+    if play_message:
+        playsound("assets/audio/main.mp3")
+'''
--- text_to_speech.py
+++ text_to_speech.py
@@ -1,36 +1,41 @@
-import boto3
-from playsound import playsound
+from elevenlabs import generate, set_api_key, save, play  # , voices
+import json
 
-with open("keys/aws_access_keys.txt", "r") as file:
-    aws_access_key_id, aws_secret_access_key, aws_session_token = (
-        *file.read().strip().split("\n"),
-        None,
-    )
+# https://github.com/elevenlabs/elevenlabs-python
+# https://beta.elevenlabs.io/speech-synthesis
+# https://docs.elevenlabs.io/voicelab/voice-design
 
-client = boto3.client(
-    "polly",
-    region_name="eu-west-2",
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    aws_session_token=aws_session_token,
-)
+with open("keys/eleven_labs_keys.txt", "r") as file:
+    keys = file.read().strip().split("\n")
 
+current_index = 0
 
-def convert_text_to_speech(message: str, play_message: bool) -> None:
-    """Convert text to speech using AWS Polly, and play the message if play_message is True"""
-    response = client.synthesize_speech(
-        Engine="neural",
-        LanguageCode="en-GB",
-        # "LexiconNames"=[ "string" ],
-        OutputFormat="mp3",
-        # "SampleRate"="string",
-        # SpeechMarkTypes"=[ "string" ],
-        Text=message,
-        TextType="text",
-        VoiceId="Amy",
-    )
-    streaming_body = response["AudioStream"]
-    with open("assets/images/main.mp3", "wb") as file:
-        file.write(streaming_body.read())
+# Stability: 0.75
+# Similarity Boost: 0.55
+
+with open("assets/files/introduction_script.json", "r") as file:
+    script_text = json.load(file)["introduction"]
+    script_text = script_text.format(name="Katrina", agent_name="Serenity")
+
+#script_text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry. I'm excited. Here's the graduate. We're very proud of you, son. A perfect report card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A little. Special day, graduation. Never thought I'd make it. Three days grade school, three days high school. Those were awkward. Three days college. I'm glad I took a day and hitchhiked around the hive. You did come back different.   - Hi, Barry. - Artie, growing a mustache? Looks good. - Hear about Frankie? - Yeah. - You going to the funeral? - No, I'm not going. Everybody knows, sting someone, you die. Don't waste it on a squirrel. Such a hothead."
+
+def convert_text_to_speech(text: str, play_message: bool, file_name: str | None = None) -> bytes:
+    global current_index
+    api_key = keys[current_index % len(keys)]
+    set_api_key(api_key)
+    audio = generate(text=text, voice="v68waeCuaiRz1jVfp33y")
+    audio_bytes = audio if isinstance(audio, bytes) else b"".join(audio)
     if play_message:
-        playsound("assets/images/main.mp3")
+        play(audio_bytes)
+    if file_name:
+        save(audio_bytes, "assets/audio/test.mp3")
+    current_index += 1
+    return audio_bytes
+
+if __name__ == "__main__":
+    import time
+    start = time.time()
+    convert_text_to_speech(script_text, False, "assets/audio/test.mp3")
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
